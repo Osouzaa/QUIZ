3c60d51f14c7382b5b477c67da8bb52853cf814f
GabrielsouzaDev <101714272+Osouzaa@users.noreply.github.com>
2023-06-07 15:56:45 -0300

    Add files via upload

---
+++ src/data/questions.js
@@ -0,0 +1,60 @@
+const data = [
+  {
+    question: "O que consta numa reta numérica?",
+    options: ["Letras", "Números", "Animais", "Cores"],
+    answer: "Números",
+  },
+  {
+    question: "Quais destes números são pares? ",
+    options: ["3", "5", "9", "8"],
+    answer: "8",
+  },
+  {
+    question: "	Quais destes números são ímpares? ",
+    options: ["7", "2", "4", "6"],
+    answer: "7",
+  },
+  {
+    question: "	Qual parte da planta serve para sustentá-la?",
+    options: ["Folha", "Caule", "Raiz", "Fruto"],
+    answer: "Caule",
+  },
+  {
+    question: "	Como se chama a parte da planta que suga os nutrientes?",
+    options: ["Folha", "Caule", "Raiz", "Fruto"],
+    answer: "length",
+  },
+  {
+    question: "	Como se chama a parte da planta que suga os nutrientes?",
+    options: ["Folha", "Caule", "Raiz", "Fruto"],
+    answer: "length",
+  },
+  {
+    question: "Qual a cor da lixeira para reciclagem do vidro? ",
+    options: ["Verde", "Amarelo", "Branco", "Azul"],
+    answer: "Verde",
+  },
+  {
+    question: "	Quanto tempo para a decomposição do plástico? ",
+    options: ["45 anos", "300 anos", "450 anos", "100 anos"],
+    answer: "450 anos",
+  },
+  {
+    question: "Qual a cor da lixeira para reciclagem de papel e papelão? ",
+    options: ["Amarelo", "Azul", "Verde", "Vermelha"],
+    answer: "Azul",
+  },
+  {
+    question: "	Qual o nome desta linha? ",
+    img: "",
+    options: ["Ondulada", "Reta", "Curva", "Mista"],
+    answer: "Ondulada",
+  },
+  {
+    question: "	O circulo é conhecido como uma linha?",
+    options: ["Curva", "Reta", "Mista", "Ondulada"],
+    answer: "Curva",
+  },
+];
+
+export default data;
