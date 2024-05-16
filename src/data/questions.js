bdc6b55bca690f0c05968d6a349cbd5b3eb491be
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-05-16 13:24:56 -0300

    changes

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,54 +1,38 @@
 const data = [
   {
-    question: "O que consta numa reta numérica?",
-    options: ["Letras", "Números", "Animais", "Cores"],
-    answer: "Números",
+    question: "O que é hardware?",
+    options: [
+      "Programas de computador",
+      "Parte física de um computador",
+      "Dados armazenados",
+      " Linguagens de programação",
+    ],
+    answer: "Parte física de um computador",
   },
   {
-    question: "Quais destes números são pares? ",
-    options: ["3", "5", "9", "8"],
-    answer: "8",
+    question: "Qual destes é um exemplo de dispositivo de entrada? ",
+    options: ["Impressora", "Teclado", "Monitor", "Caixa de som"],
+    answer: "Teclado",
   },
   {
-    question: "	Quais destes números são ímpares? ",
-    options: ["7", "2", "4", "6"],
-    answer: "7",
+    question: "O que é um software? ",
+    options: [
+      "Parte lógica de um computador",
+      "Parte Física de um computador",
+      "Dispositivos de entrada",
+      "Dados processados",
+    ],
+    answer: "Parte lógica de um computador",
   },
   {
-    question: "	Qual parte da planta serve para sustentá-la?",
-    options: ["Folha", "Caule", "Raiz", "Fruto"],
-    answer: "Caule",
+    question: "Qual desses não é um dispositivo de entrada?",
+    options: ["Mouse", "Microfone", "Monitor", "Scanner"],
+    answer: "Monitor",
   },
   {
-    question: "	Como se chama a parte da planta que suga os nutrientes?",
-    options: ["Folha", "Caule", "Raiz", "Fruto"],
-    answer: "Raiz",
-  },
-  {
-    question: "Qual a cor da lixeira para reciclagem do vidro? ",
-    options: ["Verde", "Amarelo", "Branco", "Azul"],
-    answer: "Verde",
-  },
-  {
-    question: "	Quanto tempo para a decomposição do plástico? ",
-    options: ["45 anos", "300 anos", "450 anos", "100 anos"],
-    answer: "450 anos",
-  },
-  {
-    question: "Qual a cor da lixeira para reciclagem de papel e papelão? ",
-    options: ["Amarelo", "Azul", "Verde", "Vermelha"],
-    answer: "Azul",
-  },
-  {
-    question: "	Qual o nome desta linha? ",
-    img: "",
-    options: ["Ondulada", "Reta", "Curva", "Mista"],
-    answer: "Ondulada",
-  },
-  {
-    question: "	O circulo é conhecido como uma linha?",
-    options: ["Curva", "Reta", "Mista", "Ondulada"],
-    answer: "Curva",
+    question: "Qual desses não é um dispositivo de saida?",
+    options: ["Monitor", "Caixa de Som", "Projetor", "Teclado"],
+    answer: "Teclado",
   },
 ];
 
--- src/data/questions_complete.js
+++ src/data/questions_complete.js
@@ -1,115 +1,47 @@
 const data = [
-  {
-    category: "HTML",
-    questions: [
-      {
-        question: "Qual tag cria um parágrafo?",
-        options: ["<p>", "<h1>", "<text>", "<ul>"],
-        answer: "<p>",
-        tip: "É uma tag de uma letra apenas",
-      },
-      {
-        question: "Qual atributo adiciona um link para a tag a?",
-        options: ["alt", "href", "src", "link"],
-        answer: "href",
-        tip: "Hyperlink Reference",
-      },
-      {
-        question: "As listas não ordenadas tem a tag de:",
-        options: ["<ol>", "<ul>", "<li>", "<list>"],
-        answer: "<ul>",
-      },
-      {
-        question: "Qual atributo deixa o input obrigatório?",
-        options: ["placeholder", "value", "required", "maxlength"],
-        answer: "required",
-      },
-      {
-        question: "A tag semântica indicada para rodapés é a:",
-        options: ["div", "main", "section", "footer"],
-        answer: "footer",
-      },
-    ],
-  },
-  {
-    category: "CSS",
-    questions: [
-      {
-        question: "Qual regra altera a cor de um elemento?",
-        options: ["color", "background-color", "font-size", "transition"],
-        answer: "color",
-        tip: "Cor em inglês",
-      },
-      {
-        question: "Para aumentar a fonte de um elemento utilizamos:",
-        options: ["font", "text-transform", "font-size", "hover"],
-        answer: "font-size",
-      },
-      {
-        question: "A posição que deixa um elemento fixo é a:",
-        options: ["static", "absolute", "fixed", "relative"],
-        answer: "fixed",
-      },
-    ],
-  },
+
   {
     category: "Jogo da Revisão",
     questions: [
       {
-        question: "O que consta numa reta numérica?",
-        options: ["Letras", "Números", "Animais", "Cores"],
-        answer: "Números",
+        question: "O que é hardware?",
+        options: [
+          "Programas de computador",
+          "Parte física de um computador",
+          "Dados armazenados",
+          " Linguagens de programação",
+        ],
+        answer: "Parte física de um computador",
       },
       {
-        question: "Quais destes números são pares? ",
-        options: ["3", "5", "9", "8"],
-        answer: "8",
+        question: "Qual destes é um dispositivo de entrada? ",
+        options: ["Impressora", "Teclado", "Monitor", "Caixa de som"],
+        answer: "Teclado",
       },
       {
-        question: "	Quais destes números são ímpares? ",
-        options: ["7", "2", "4", "6"],
-        answer: "7",
+        question: "O que é um software? ",
+        options: [
+          "Parte lógica de um computador",
+          "Parte Física de um computador",
+          "Dispositivos de entrada",
+          "Dados processados",
+        ],
+        answer: "Parte lógica de um computador",
       },
       {
-        question: "	Qual parte da planta serve para sustentá-la?",
-        options: ["Folha", "Caule", "Raiz", "Fruto"],
-        answer: "Caule",
+        question: "Qual desses não é um dispositivo de entrada?",
+        options: ["Mouse", "Microfone", "Monitor", "Scanner"],
+        answer: "Monitor",
       },
       {
-        question: "	Como se chama a parte da planta que suga os nutrientes?",
-        options: ["Folha", "Caule", "Raiz", "Fruto"],
-        answer: "length",
+        question: "Qual desses não é um dispositivo de saida?",
+        options: ["Monitor", "Caixa de Som", "projetor", "Teclado"],
+        answer: "Teclado",
       },
       {
-        question: "	Como se chama a parte da planta que suga os nutrientes?",
-        options: ["Folha", "Caule", "Raiz", "Fruto"],
-        answer: "length",
-      },
-      {
-        question: "Qual a cor da lixeira para reciclagem do vidro? ",
-        options: ["Verde", "Amarelo", "Branco", "Azul"],
-        answer: "Verde",
-      },
-      {
-        question: "	Quanto tempo para a decomposição do plástico? ",
-        options: ["45 anos", "300 anos", "450 anos", "100 anos"],
-        answer: "450 anos",
-      },
-      {
-        question: "Qual a cor da lixeira para reciclagem de papel e papelão? ",
-        options: ["Amarelo", "Azul", "Verde", "Vermelha"],
-        answer: "Azul",
-      },
-      {
-        question: "	Qual o nome desta linha? ",
-        img: "",
-        options: ["Ondulada", "Reta", "Curva", "Mista"],
-        answer: "Ondulada",
-      },
-      {
-        question: "	O circulo é conhecido como uma linha?",
-        options: ["Curva", "Reta", "Mista", "Ondulada"],
-        answer: "Curva",
+        question: "O Monitor é considerado qual tipo de disposivo?",
+        options: ["Dispositivo de Saída", "Dispositivos de Entrada"],
+        answer: "Dispositivo de Saída",
       },
     ],
   },
